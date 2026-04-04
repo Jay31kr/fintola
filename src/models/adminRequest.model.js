bcdd1bea31dd127d357149ce85fb656253515838
jay <guptajay3103@gmail.com>
2026-04-04 11:44:10 +0530

    added AdminRequest model

---
+++ src/models/adminRequest.model.js
@@ -0,0 +1,35 @@
+import mongoose , {Schema}  from "mongoose";
+
+const adminRequestSchema = new Schema({
+    user  :{
+        type : mongoose.Schema.Types.ObjectId,
+        ref : "User",
+        required :true,
+    },
+
+    status : {
+        type: String,
+        enum : ["pending" , "approved" , "rejected"],
+        default :"pending",
+    },
+
+    requestedAt : {
+        type: Date,
+        default : Date.now,
+    },
+
+    reviewedBy : {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+    },
+
+    reviewedAt : {
+        type : Date,
+        default : Date.now,
+    }
+});
+
+adminRequestSchema.index({ user: 1 });
+adminRequestSchema.index({ status: 1 });
+
+export const AdminRequest = mongoose.model("AdminRequest" , adminRequestSchema);
