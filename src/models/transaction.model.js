3b2dc4d486491474a1ecd799cc6b4784d9732a9e
jay <guptajay3103@gmail.com>
2026-04-04 12:30:31 +0530

    added Transaction model

---
+++ src/models/transaction.model.js
@@ -0,0 +1,46 @@
+import mongoose ,  {Schema} from "mongoose";
+
+const transactionSchema =  new Schema({
+    title : {
+        type: String,
+        required: true,
+    },
+
+    amount : {
+        type : Number,
+        required : true,
+    },
+
+    type: {
+        type: String,
+        enum: ["expense" , "income"],
+        required : true,
+    },
+
+    category: {
+        type: String,
+        enum: ["marketing" , "HRExpense" , "Utilities" , "payroll" , "others"],
+        required:true,
+    },
+
+    status : {
+        type : String,
+        enum:["pending" , "approved" , "reject"],
+        default : "pending",
+    },
+
+    date: {
+    type: Date,
+    required: true
+    },
+
+    createdBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true
+  },
+},{
+    timeStamps :true,
+});
+
+export const Trnasaction = mongoose.model("Transaction" , transactionSchema);
