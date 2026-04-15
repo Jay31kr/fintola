837746b8854e8727127e99ecf9485e03c38308cc
Jay Kumar Gupta <112371389+Jay31kr@users.noreply.github.com>
2026-04-15 17:25:31 +0530

    Fix CORS configuration in app.js

--- src/app.js
+++ src/app.js
@@ -10,9 +10,9 @@ import adminRoutes from "./routes/admin.routes.js"
 const app = express();
 
 app.use(cors({
-    origin: process.env.CLIENT_URL || true,
-    credentials: true
-}))
+  origin: process.env.CLIENT_URL,
+  credentials: true,
+}));
 
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
